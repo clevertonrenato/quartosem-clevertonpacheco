04428edb8c382051265c93e0b380dfa5bc558900
c.renatodp@gmail.com <c.renatodp@gmail.com@37e79bc8-fd6f-4e20-e8be-16a92df6d2af>
2015-03-06 23:24:33 +0000

    trabalho dia 06/03/2015

---
+++ TCC/TCC/src/java/modelo/Pedido.java
@@ -0,0 +1,40 @@
+
+package modelo;
+
+import java.util.Date;
+
+
+public class Pedido {
+    private int numero;
+    private Date data;
+    private float valor;
+
+    public Pedido() {
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+    
+    
+}
