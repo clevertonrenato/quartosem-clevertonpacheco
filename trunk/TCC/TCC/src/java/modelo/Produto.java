a3081be0b6e55fd5a819851cb83d1f44211820c0
c.renatodp@gmail.com <c.renatodp@gmail.com@37e79bc8-fd6f-4e20-e8be-16a92df6d2af>
2015-03-06 23:24:33 +0000

    trabalho dia 06/03/2015

---
+++ trunk/TCC/TCC/src/java/modelo/Produto.java
@@ -0,0 +1,47 @@
+
+package modelo;
+
+
+public class Produto {
+    private int codigo;
+    private String nome;
+    private float preco;
+    private String descricao;
+
+    public Produto() {
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    
+}
